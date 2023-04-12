7027718de0401aff1b2efe120baa365eeba6244d
JOSE AMARIS <jamaris@corrosioncic.com>
2023-04-12 14:34:49 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  cities= ['Madrid', 'Paris', 'Lima']
+  name!: string;
   title = 'reto01';
 }
